ca732a3b578391cba8dff1afae47b35a2177c073
Pablo <20116813+rulyone@users.noreply.github.com>
2025-07-16 16:15:56 -0400

    added caching of uploaded images

--- src/main/java/dev/quantumentangled/blog/config/WebConfig.java
+++ src/main/java/dev/quantumentangled/blog/config/WebConfig.java
@@ -1,7 +1,10 @@
 package dev.quantumentangled.blog.config;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,6 +17,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:" + uploadDir + "/");
+                .addResourceLocations("file:" + uploadDir + "/")
+                .setCacheControl(
+                        CacheControl
+                                .maxAge(365, TimeUnit.DAYS)
+                                .cachePublic()
+                                .immutable());
     }
 }
